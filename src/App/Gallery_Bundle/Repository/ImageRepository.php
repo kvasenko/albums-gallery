3a5d9a1a935163c2b38a6a351b31794b2257848e
kvasenko <kvasenko@ukr.net>
2016-10-28 12:39:34 +0300

    getQueryImagesByAlbum method added

--- src/App/Gallery_Bundle/Repository/ImageRepository.php
+++ src/App/Gallery_Bundle/Repository/ImageRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Gallery_Bundle\Repositories;
+namespace App\Gallery_Bundle\Repository;
 
 use Doctrine\ORM\QueryBuilder;
 
@@ -9,5 +9,12 @@ use Doctrine\ORM\EntityRepository;
 
 class ImageRepository extends EntityRepository
 {
+    public function getQueryImagesByAlbum($albumId)
+    {
+        $query = 'SELECT i FROM AppGallery_Bundle:Image i WHERE i.album = :id ORDER BY i.id ASC';
 
+        return $this->getEntityManager()
+            ->createQuery($query)
+            ->setParameter('id', $albumId);
+    }
 }
